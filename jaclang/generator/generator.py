4d7031359dbe1d41a25878a950cf86c6643cf564
Jakob Zorz <54270248+Zorz42@users.noreply.github.com>
2022-09-27 15:55:38 +0200

    added better instruction printing

--- jaclang/generator/generator.py
+++ jaclang/generator/generator.py
@@ -114,66 +114,141 @@ class AddInstruction(Instruction):
 class SubInstruction(Instruction):
     def __init__(self, reg_a: RegisterParameter, reg_b: RegisterParameter, reg_save: RegisterParameter):
         super().__init__("SUB", 0b00010, [reg_a, reg_b, reg_save], 4)
+        self.reg_a = reg_a
+        self.reg_b = reg_b
+        self.reg_save = reg_save
+
+    def printInfo(self):
+        print(f"    {self.reg_save.getInfo()} = {self.reg_a.getInfo()} - {self.reg_b.getInfo()}")
 
 
 class BslInstruction(Instruction):
     def __init__(self, reg_a: RegisterParameter, reg_b: RegisterParameter, reg_save: RegisterParameter):
         super().__init__("BSL", 0b00011, [reg_a, reg_b, reg_save], 4)
+        self.reg_a = reg_a
+        self.reg_b = reg_b
+        self.reg_save = reg_save
+
+    def printInfo(self):
+        print(f"    {self.reg_save.getInfo()} = {self.reg_a.getInfo()} << {self.reg_b.getInfo()}")
 
 
 class BsrInstruction(Instruction):
     def __init__(self, reg_a: RegisterParameter, reg_b: RegisterParameter, reg_save: RegisterParameter):
         super().__init__("BSR", 0b00100, [reg_a, reg_b, reg_save], 4)
+        self.reg_a = reg_a
+        self.reg_b = reg_b
+        self.reg_save = reg_save
+
+    def printInfo(self):
+        print(f"    {self.reg_save.getInfo()} = {self.reg_a.getInfo()} >> {self.reg_b.getInfo()}")
 
 
 class OrInstruction(Instruction):
     def __init__(self, reg_a: RegisterParameter, reg_b: RegisterParameter, reg_save: RegisterParameter):
         super().__init__("OR", 0b00101, [reg_a, reg_b, reg_save], 4)
+        self.reg_a = reg_a
+        self.reg_b = reg_b
+        self.reg_save = reg_save
+
+    def printInfo(self):
+        print(f"    {self.reg_save.getInfo()} = {self.reg_a.getInfo()} OR {self.reg_b.getInfo()}")
 
 
 class AndInstruction(Instruction):
     def __init__(self, reg_a: RegisterParameter, reg_b: RegisterParameter, reg_save: RegisterParameter):
         super().__init__("AND", 0b00111, [reg_a, reg_b, reg_save], 4)
+        self.reg_a = reg_a
+        self.reg_b = reg_b
+        self.reg_save = reg_save
+
+    def printInfo(self):
+        print(f"    {self.reg_save.getInfo()} = {self.reg_a.getInfo()} AND {self.reg_b.getInfo()}")
 
 
 class XorInstruction(Instruction):
     def __init__(self, reg_a: RegisterParameter, reg_b: RegisterParameter, reg_save: RegisterParameter):
         super().__init__("XOR", 0b00110, [reg_a, reg_b, reg_save], 4)
+        self.reg_a = reg_a
+        self.reg_b = reg_b
+        self.reg_save = reg_save
+
+    def printInfo(self):
+        print(f"    {self.reg_save.getInfo()} = {self.reg_a.getInfo()} XOR {self.reg_b.getInfo()}")
 
 
 class NotInstruction(Instruction):
     def __init__(self, reg_a: RegisterParameter, reg_save: RegisterParameter):
         super().__init__("NOT", 0b01000, [reg_a, EmptyByteParameter(), reg_save], 4)
+        self.reg_a = reg_a
+        self.reg_save = reg_save
+
+    def printInfo(self):
+        print(f"    {self.reg_save.getInfo()} = NOT {self.reg_a.getInfo()}")
 
 
 class XnorInstruction(Instruction):
     def __init__(self, reg_a: RegisterParameter, reg_b: RegisterParameter, reg_save: RegisterParameter):
         super().__init__("XNOR", 0b01001, [reg_a, reg_b, reg_save], 4)
+        self.reg_a = reg_a
+        self.reg_b = reg_b
+        self.reg_save = reg_save
+
+    def printInfo(self):
+        print(f"    {self.reg_save.getInfo()} = {self.reg_a.getInfo()} XNOR {self.reg_b.getInfo()}")
 
 
 class NandInstruction(Instruction):
     def __init__(self, reg_a: RegisterParameter, reg_b: RegisterParameter, reg_save: RegisterParameter):
         super().__init__("NAND", 0b01010, [reg_a, reg_b, reg_save], 4)
+        self.reg_a = reg_a
+        self.reg_b = reg_b
+        self.reg_save = reg_save
+
+    def printInfo(self):
+        print(f"    {self.reg_save.getInfo()} = {self.reg_a.getInfo()} NAND {self.reg_b.getInfo()}")
 
 
 class MemwInstruction(Instruction):
     def __init__(self, reg_addr: RegisterParameter, addr_offset: int, reg_value: RegisterParameter):
         super().__init__("MEMW", 0b01011, [reg_addr, reg_value, Value8Parameter(addr_offset)], 4)
+        self.reg_addr = reg_addr
+        self.addr_offset = addr_offset
+        self.reg_value = reg_value
+
+    def printInfo(self):
+        print(f"    [{self.reg_addr.getInfo()} + {self.addr_offset}] = {self.reg_value.getInfo()}")
 
 
 class MemrInstruction(Instruction):
     def __init__(self, reg_addr: RegisterParameter, addr_offset: int, reg_save: RegisterParameter):
         super().__init__("MEMR", 0b01100, [reg_addr, Value8Parameter(addr_offset), reg_save], 4)
+        self.reg_addr = reg_addr
+        self.addr_offset = addr_offset
+        self.reg_save = reg_save
+
+    def printInfo(self):
+        print(f"    {self.reg_save.getInfo()} = [{self.reg_addr.getInfo()} + {self.addr_offset}]")
 
 
 class ImmediateInstruction(Instruction):
     def __init__(self, reg_save: RegisterParameter, value: int):
         super().__init__("IMM", 0b01101, [reg_save, Value16Parameter(value)], 4)
+        self.reg_save = reg_save
+        self.value = value
+
+    def printInfo(self):
+        print(f"    {self.reg_save.getInfo()} = {self.value}")
 
 
 class MovInstruction(Instruction):
     def __init__(self, reg_a: RegisterParameter, reg_save: RegisterParameter):
         super().__init__("MOV", 0b01110, [reg_a, EmptyByteParameter(), reg_save], 4)
+        self.reg_a = reg_a
+        self.reg_save = reg_save
+
+    def printInfo(self):
+        print(f"    {self.reg_save.getInfo()} = {self.reg_a.getInfo()}")
 
 
 class CmpInstruction(Instruction):
@@ -209,16 +284,27 @@ class PopInstruction(Instruction):
 class SetSpInstruction(Instruction):
     def __init__(self, reg: RegisterParameter):
         super().__init__("SETSP", 0b10101, [reg], 2)
+        self.reg = reg
 
+    def printInfo(self):
+        print(f"    SP = {self.reg.getInfo()}")
 
 class GetSpInstruction(Instruction):
     def __init__(self, reg: RegisterParameter):
         super().__init__("GETSP", 0b10110, [EmptyByteParameter(), EmptyByteParameter(), reg], 4)
+        self.reg = reg
+
+    def printInfo(self):
+        print(f"    {self.reg.getInfo()} = SP")
 
 
 class GetPcInstruction(Instruction):
     def __init__(self, reg: RegisterParameter):
         super().__init__("GETPC", 0b10111, [EmptyByteParameter(), EmptyByteParameter(), reg], 4)
+        self.reg = reg
+
+    def printInfo(self):
+        print(f"    {self.reg.getInfo()} = PC")
 
 
 class PushaInstruction(Instruction):
