ce109a24ea01fd4775a2051f6c3967366e2534e9
Jakob Zorz <54270248+Zorz42@users.noreply.github.com>
2022-09-27 16:07:58 +0200

    added command line options

--- jaclang/__main__.py
+++ jaclang/__main__.py
@@ -8,20 +8,28 @@ from jaclang.preprocessor import preprocess
 
 
 def main():
-    if len(sys.argv) != 2:
-        print("Usage: python3 -m jaclang [input_file]")
+    if len(sys.argv) < 2:
+        print(
+            """Usage: python3 -m jaclang [input_file] [options]
+Options:
+- debug_preprocess: print preprocessed code
+- debug_tokens: print tokens
+- debug_tree: print abstract syntax tree
+- debug_assembly: print generated assembly code"""
+        )
         return
 
     input_file = sys.argv[1]
+    options = sys.argv[2:]
 
     with open(input_file, "r") as file:
         file_contents = file.read()
 
     try:
-        preprocessed_contents = preprocess(file_contents, False)
-        tokens = tokenize(preprocessed_contents, False)
-        instructions = parse(tokens, True)
-        binary_code = generate(instructions, True)
+        preprocessed_contents = preprocess(file_contents, "debug_preprocess" in options)
+        tokens = tokenize(preprocessed_contents, "debug_tokens" in options)
+        instructions = parse(tokens, "debug_tree" in options)
+        binary_code = generate(instructions, "debug_assembly" in options)
 
         print(f"Binary code size: {len(binary_code)} bytes")
     except JaclangSyntaxError as error:
