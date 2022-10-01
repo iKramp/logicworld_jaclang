cb81db943c342ebcfae56644e51893953b89a53b
Jakob Zorz <54270248+Zorz42@users.noreply.github.com>
2022-10-01 16:20:46 +0200

    added fibonacci test

---
+++ jaclang_test/tests/19_fibonacci.jl
@@ -0,0 +1,21 @@
+/// expect 55
+
+/// fibonacci with ugly global variable workaround
+
+var param
+
+func fib() {
+    if (param == 0) | (param == 1)
+        return param
+    var par = param
+    param = par - 1
+    var a = fib()
+    param = par - 2
+    var b = fib()
+    return a + b
+}
+
+func main() {
+    param = 10
+    return fib()
+}
