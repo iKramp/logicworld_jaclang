90ae8b5f2940a87678e64e9d3b3633f255c709dd
Jakob Zorz <54270248+Zorz42@users.noreply.github.com>
2022-10-01 20:36:23 +0200

    removed a warning when running jaclang from terminal

--- jaclang/__init__.py
+++ jaclang/__init__.py
@@ -1,1 +1,11 @@
-from .__main__ import compileJaclang
+from jaclang.generator import generate
+from jaclang.lexer import tokenize
+from jaclang.parser import parse
+from jaclang.preprocessor import preprocess
+
+
+def compileJaclang(file_contents: str, options: list[str]) -> list[int]:
+    preprocessed_contents = preprocess(file_contents, "debug_preprocess" in options)
+    tokens = tokenize(preprocessed_contents, "debug_tokens" in options)
+    instructions = parse(tokens, "debug_tree" in options)
+    return generate(instructions, "debug_assembly" in options)
--- jaclang/__main__.py
+++ jaclang/__main__.py
@@ -1,17 +1,7 @@
 import sys
 
+from jaclang import compileJaclang
 from jaclang.error.syntax_error import JaclangSyntaxError
-from jaclang.generator import generate
-from jaclang.lexer import tokenize
-from jaclang.parser import parse
-from jaclang.preprocessor import preprocess
-
-
-def compileJaclang(file_contents: str, options: list[str]) -> list[int]:
-    preprocessed_contents = preprocess(file_contents, "debug_preprocess" in options)
-    tokens = tokenize(preprocessed_contents, "debug_tokens" in options)
-    instructions = parse(tokens, "debug_tree" in options)
-    return generate(instructions, "debug_assembly" in options)
 
 
 def main():
